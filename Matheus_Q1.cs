12a4e660ed57bae49d81b3edd90e47ebc0e49543
Matheus-Siqueira-Pinto <56691969+Matheus-Siqueira-Pinto@users.noreply.github.com>
2019-10-17 13:21:56 -0300

    Add files via upload

---
+++ Matheus_Q1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+
+namespace programaProva
+{
+    
+class Program
+{
+        static void Main(string[] args)
+        {
+            string text, n;
+            int number, count;
+
+            Console.WriteLine("digite quantas palavras ou frases serão testadas: \n");
+            n = Console.ReadLine();
+
+            number = int.Parse(n);
+            count = 0;
+
+
+            // Loop com os textos
+            while (count < number)
+            {
+               
+                count = count + 1;
+
+                Console.WriteLine("\n \n Qual a frase ou palavra {0} a ser testada? \n", (count));
+                text = Console.ReadLine();
+
+
+
+                //Printa o resultado
+                if (IsPalindrome(text))
+                {
+                    //Console.WriteLine(test);
+                    Console.WriteLine("\n {0} É PALÍNDROMO", text);
+                }
+                else
+                {
+                   // Console.WriteLine(test);
+                    Console.WriteLine("\n {0} NÃO É PALÍNDROMO", text);
+                }
+            }
+           
+        }
+
+        private static bool IsPalindrome(string text)
+        {
+            //Separa em caracteres e inverte.
+            char[] letters = text.ToCharArray();
+            Array.Reverse(letters);
+
+            //Transforma os caracteres em palavras
+            string reverseWord = new string(letters);
+
+            //Transforma tudo para minúsculas e testa
+            if (RemoveOthers(text.ToLower()) == RemoveOthers(reverseWord.ToLower()))
+                return true;
+            else
+                return false;
+        }
+
+        //Remove acentuação
+        private static string RemoveOthers(string text)
+        {
+            text = text.Replace("À", "a");
+            text = text.Replace("Â", "a");
+            text = text.Replace("Á", "a");
+            text = text.Replace("Ã", "a");
+            
+            text = text.Replace("à", "a");
+            text = text.Replace("â", "a");
+            text = text.Replace("á", "a");
+            text = text.Replace("ã", "a");
+            
+            text = text.Replace("Ê", "e");
+            text = text.Replace("É", "e");
+            
+            text = text.Replace("ê", "e");
+            text = text.Replace("é", "e");
+
+            text = text.Replace("I", "i");
+
+            text = text.Replace("Ó", "o");
+            text = text.Replace("Ô", "o");
+            
+            text = text.Replace("ó", "o");
+            text = text.Replace("ô", "o");
+
+            text = text.Replace("U", "u");
+            text = text.Replace("Ú", "u");
+
+            text = text.Replace("ú", "u");
+
+            text = text.Replace(" ", "");
+            text = text.Replace(".", "");
+            text = text.Replace(",", "");
+            text = text.Replace("!", "");
+            text = text.Replace("?", "");
+            text = text.Replace(";", "");
+
+
+            return text;
+        }
+    }
+}
---
+++ Matheus_Q2.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prova_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text, n;
+            int number, count, lentext, lentext4, numeroas, palavrasas;
+
+            Console.WriteLine("digite quantas palavras serão testadas: \n");
+            n = Console.ReadLine();
+
+            number = int.Parse(n);
+            count = 0;
+            numeroas = 0;
+            lentext = 0;
+            lentext4 = 0;
+            palavrasas = 0;
+
+            List<string> palavras;
+            palavras = new List<string>();
+
+
+            // Loop com os textos
+            while (count < number)
+            {
+
+                count = count + 1;
+
+                Console.WriteLine("\n \n Qual a palavra {0} a ser testada? \n", (count));
+                text = Console.ReadLine();
+
+                //Passando pra minusculas
+                text = text.ToLower();
+
+                //Contando a quantidade de as
+                numeroas = text.Length - text.Replace("a", "").Length;
+                if (numeroas >= 3)
+                {
+                    palavrasas += 1;
+                }
+
+
+                //Verificando o tamanho
+                lentext = text.Length;
+
+                if (lentext >= 4)
+                {
+                    lentext4 += 1;
+                }
+
+
+                //Adicionando pra lista
+                palavras.Add(text);
+            }
+
+            //Ordenando alfabeticamente
+            palavras.Sort();
+
+            
+            //Printando tudo e gg
+            Console.WriteLine("\n \n A quantidade de palavras com a letra 'a' é: {0} \n", palavrasas );
+            Console.WriteLine("\n \n A quantidade de palavras com mais de 4 letras é: {0} \n", lentext4);
+            Console.WriteLine("\n \n A lista de palavras em ordem alfabética: \n ");
+            foreach (var item in palavras)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
